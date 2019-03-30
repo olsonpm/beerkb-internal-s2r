8e2409d82ce7aee345097ec3f7bce7ec81b6a75d
Philip Olson <philip.olson@pm.me>
2019-03-30 11:58:45 -0500

    add prettier and eslint
    - prettify server.js formatting
    - disable eslint no-console

--- server.js
+++ server.js
@@ -1,91 +1,91 @@
-'use strict';
-
+'use strict'
 
 //---------//
 // Imports //
 //---------//
 
-const bPromise = require('bluebird')
-  , chalk = require('chalk')
-  , childProcess = require('child_process')
-  , fs = require('fs')
-  , Koa = require('koa')
-  , moment = require('moment')
-  , path = require('path')
-  , portfinder = require('portfinder')
-  , { attachDestroyMethod } = require('server-destroy-promise')
-  , sqliteToRest = require('sqlite-to-rest')
-  ;
-
-
+const bPromise = require('bluebird'),
+  chalk = require('chalk'),
+  childProcess = require('child_process'),
+  fs = require('fs'),
+  Koa = require('koa'),
+  moment = require('moment'),
+  path = require('path'),
+  portfinder = require('portfinder'),
+  { attachDestroyMethod } = require('server-destroy-promise'),
+  sqliteToRest = require('sqlite-to-rest')
 //------//
 // Init //
 //------//
 
-const bGetPort = bPromise.promisify(portfinder.getPort)
-  , dbPath = path.join(__dirname, 'beer.sqlite3')
-  , getSqliteRouter = sqliteToRest.getSqliteRouter
-  , highlight = chalk.green
-  ;
-
-let server;
-
+const bGetPort = bPromise.promisify(portfinder.getPort),
+  dbPath = path.join(__dirname, 'beer.sqlite3'),
+  getSqliteRouter = sqliteToRest.getSqliteRouter,
+  highlight = chalk.green
+let server
 
 //------//
 // Main //
 //------//
 
-const app = new Koa();
+const app = new Koa()
 
-const run = () => bGetPort()
-  .then(port => {
+const run = () =>
+  bGetPort()
+    .then(port => {
+      // assuming run only happens once
+      initDailyDbReset()
 
-    // assuming run only happens once
-    initDailyDbReset();
+      return bPromise.props({
+        router: getSqliteRouter({ dbPath }),
+        port,
+      })
+    })
+    .then(({ router, port }) => {
+      server = app
+        .use(router.routes())
+        .use(router.allowedMethods())
+        .listen(port)
 
-    return bPromise.props({
-      router: getSqliteRouter({ dbPath })
-      , port
-    });
-  })
-  .then(({ router, port }) => {
-    server = app.use(router.routes())
-      .use(router.allowedMethods())
-      .listen(port);
+      attachDestroyMethod(server)
 
-    attachDestroyMethod(server);
+      // eslint-disable-next-line no-console
+      console.log(
+        'beerkb-internal-s2r server listening on port: ' + highlight(port)
+      )
 
-    console.log('beerkb-internal-s2r server listening on port: ' + highlight(port));
-
-    return { server, port };
-  });
-
+      return { server, port }
+    })
 
 //-------------//
 // Helper Fxns //
 //-------------//
 
 function initDailyDbReset() {
-  const curFile = dbPath
-    , logFile = path.join(__dirname, 'reset.log')
-    , msTilMidnight =  moment().add(1, 'day').startOf('day').diff(moment())
-    , resetFile = path.join(__dirname, 'beer.reset.sqlite3')
-    ;
-
+  const curFile = dbPath,
+    logFile = path.join(__dirname, 'reset.log'),
+    msTilMidnight = moment()
+      .add(1, 'day')
+      .startOf('day')
+      .diff(moment()),
+    resetFile = path.join(__dirname, 'beer.reset.sqlite3')
   setTimeout(() => {
-    resetDb();
-    setInterval(resetDb, 86400000);
-  }, msTilMidnight);
+    resetDb()
+    setInterval(resetDb, 86400000)
+  }, msTilMidnight)
 
   function resetDb() {
-    const command = 'cp ' + resetFile + ' ' + curFile
-      , cb = (err, stdout, stderr) => {
-        if (err) fs.writeFile(logFile, err.toString(), noop);
-        else fs.writeFile(logFile, 'stdout: ' + stdout + '\n\nstderr: ' + stderr + '\n', noop);
+    const command = 'cp ' + resetFile + ' ' + curFile,
+      cb = (err, stdout, stderr) => {
+        if (err) fs.writeFile(logFile, err.toString(), noop)
+        else
+          fs.writeFile(
+            logFile,
+            'stdout: ' + stdout + '\n\nstderr: ' + stderr + '\n',
+            noop
+          )
       }
-      ;
-
-    childProcess.exec(command, cb);
+    childProcess.exec(command, cb)
   }
 }
 
@@ -95,4 +95,4 @@ function noop() {}
 // Exports //
 //---------//
 
-module.exports = { run };
+module.exports = { run }
